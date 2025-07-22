c2caec4bb31a54055e8d4ec90ef4918b2f8aaa0b
miezlearning <gottfriedemptiness@gmail.com>
2025-07-22 16:33:44 +0800

    ✨ feat: update welcome page to comment out login and register links for testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,9 @@
     <h1 class="text-4xl font-bold mb-4">Selamat Datang di MoodTracker</h1>
     <p class="text-lg text-gray-700 mb-8">Catat suasana hati dan pikiran Anda setiap hari dengan mudah.</p>
     @guest
-    <a href="{{ route('login') }}" class="bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600">Login</a>
-    <a href="{{ route('register') }}" class="ml-4 bg-gray-500 text-white px-6 py-3 rounded hover:bg-gray-600">Register</a>
+    <!-- <a href="{{ route('login') }}" class="bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600">Login</a>
+    <a href="{{ route('register') }}" class="ml-4 bg-gray-500 text-white px-6 py-3 rounded hover:bg-gray-600">Register</a> -->
+    <a>test </a>
     @else
     <a href="{{ route('entries.index') }}" class="bg-green-500 text-white px-6 py-3 rounded hover:bg-green-600">My Journal</a>
     @endguest
